d6c4c31d571699fd8288a16e10c0dc8c1388441e
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-07-31 20:07:51 +0200

    Add project files.

---
+++ XFrame.Caching/CacheKey.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using XFrame.Common.Extensions;
+using XFrame.ValueObjects.SingleValueObjects;
+
+namespace XFrame.Caching
+{
+    [JsonConverter(typeof(SingleValueObjectConverter))]
+    public class CacheKey : SingleValueObject<string>
+    {
+        public const int MaxLength = 256;
+
+        public static CacheKey With(params string[] keys)
+        {
+            return new CacheKey(string.Join("-", keys));
+        }
+
+        public static CacheKey With(Type ownerType, params string[] keys)
+        {
+            return With($"{ownerType.GetCacheKey()}:{string.Join("-", keys)}");
+        }
+
+        public CacheKey(string value) : base(value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            if (value.Length > MaxLength)
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"Cache keys can maximum be '{MaxLength}' in length");
+        }
+    }
+}
